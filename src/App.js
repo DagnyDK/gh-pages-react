4e89884c4d04e31a64b5d7145961fcd1a0415e47
Dagny Kight <dagnykight@gmail.com>
2021-05-26 11:19:57 -0400

    update page

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          React is fun to learn
         </a>
       </header>
     </div>
